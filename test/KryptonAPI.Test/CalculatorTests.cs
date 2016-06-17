7e74faba82870e007fe221bd259e7a14d666cb43
Nilur <mrnilur@gmail.com>
2016-06-17 22:02:54 +0100

    Added moq, fixed gitignore for test project

--- test/KryptonAPI.Test/CalculatorTests.cs
+++ test/KryptonAPI.Test/CalculatorTests.cs
@@ -1,3 +1,5 @@
+using KryptonAPI.UnitOfWork;
+using Moq;
 using NUnit.Framework;
 
 namespace KryptonAPI.Test
@@ -5,12 +7,21 @@ namespace KryptonAPI.Test
     [TestFixture]
     public class CalculatorTests
     {
-        [TestCase(1, 1, 2)]
-        [TestCase(-1, -1, -2)]
-        [TestCase(100, 5, 105)]
-        public void CanAddNumbers(int x, int y, int expected)
-        {
-            Assert.That(1 + 1, Is.EqualTo(2));
+        // [TestCase(1, 1, 2)]
+        // [TestCase(-1, -1, -2)]
+        // [TestCase(100, 5, 105)]
+        // public void CanAddNumbers(int x, int y, int expected)
+        // {
+        //     Assert.That(1 + 1, Is.EqualTo(2));
+        // }
+
+        [Test]
+        public void MyTest(){
+            var mymock = new Mock<IUnitOfWork>();
+
+            mymock.Object.SaveChanges();
+
+            mymock.Verify(m => m.SaveChanges(), Times.Exactly(1));
         }
     }
 }
