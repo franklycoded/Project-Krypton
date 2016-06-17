b2e3ac659eb19cfb6ad1c5e0f8532da725ae68b0
Nilur <mrnilur@gmail.com>
2016-06-17 23:27:14 +0100

    Added tests for UnitOfWorkScope

--- test/KryptonAPI.Test/UnitOfWorkScopeTests.cs
+++ test/KryptonAPI.Test/UnitOfWorkScopeTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using KryptonAPI.UnitOfWork;
 using Moq;
 using NUnit.Framework;
@@ -58,6 +60,30 @@ namespace KryptonAPI.Test
 
             testContext1.Verify(c => c.SaveChanges(), Times.Once);
             testContext2.Verify(c => c.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void Test_TwoContexts_SaveChangesAsync_CalledOnEachContext(){
+            var testContext1 = new Mock<IUnitOfWorkContext>();
+            var testContext2 = new Mock<IUnitOfWorkContext>();
+
+            testContext1.Setup(m => m.SaveChangesAsync()).Returns(Task.Factory.StartNew(() => {
+            }));
+            testContext2.Setup(m => m.SaveChangesAsync()).Returns(Task.Factory.StartNew(() => {
+            }));
+
+            _mockUnitOfWorkContextFactory.Setup(m => m.GetContext<bool>()).Returns(testContext1.Object);
+            _mockUnitOfWorkContextFactory.Setup(m => m.GetContext<int>()).Returns(testContext2.Object);
+
+            var uow = new UnitOfWorkScope(_mockUnitOfWorkContextFactory.Object);
+
+            uow.GetContext<bool>();
+            uow.GetContext<int>();
+
+            uow.SaveChangesAsync().Wait();
+
+            testContext1.Verify(c => c.SaveChangesAsync(), Times.Once);
+            testContext2.Verify(c => c.SaveChangesAsync(), Times.Once);
         }
     }
 }
