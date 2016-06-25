ed97a92ae52bc80756c9a19a1170087dac603012
Nilur <mrnilur@gmail.com>
2016-06-25 14:46:38 +0100

    Added test for GetById in CRUDManager

--- test/KryptonAPI.Test/Service/CRUDManagerTests.cs
+++ test/KryptonAPI.Test/Service/CRUDManagerTests.cs
@@ -52,6 +52,47 @@ namespace KryptonAPI.Test.Service
             });
         }
 
-        
+        [Test]
+        public void Test_NoRepository_ArgumentNullException(){
+            Assert.Throws<ArgumentNullException>(() => {
+                var manager = new CRUDManager<TestContext, TestEntity, TestDto>(_mockUnitOfWork.Object, null, _mockDataContractMapper.Object);
+            });
+        }
+
+        [Test]
+        public void Test_NoDtoMapper_ArgumentNullException(){
+            Assert.Throws<ArgumentNullException>(() => {
+                var manager = new CRUDManager<TestContext, TestEntity, TestDto>(_mockUnitOfWork.Object, _mockRepository.Object, null);
+            });
+        }
+
+        [Test]
+        public void Test_GetById_EntityNotFound_ReturnNull(){
+            _mockRepository.Setup(m => m.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(null);
+
+            var manager = new CRUDManager<TestContext, TestEntity, TestDto>(_mockUnitOfWork.Object, _mockRepository.Object, _mockDataContractMapper.Object);
+
+            var result = manager.GetById(1);
+
+            Assert.IsNull(result.Result);
+        }
+
+        [Test]
+        public void Test_GetById_EntityFound_Mapped(){
+            var testEntity = new TestEntity();
+            var testDto = new TestDto();
+
+            _mockRepository.Setup(m => m.GetByIdAsync(1)).ReturnsAsync(testEntity);
+
+            _mockDataContractMapper.Setup(m => m.MapEntityToDto(testEntity)).Returns(testDto);
+
+            var manager = new CRUDManager<TestContext, TestEntity, TestDto>(_mockUnitOfWork.Object, _mockRepository.Object, _mockDataContractMapper.Object);
+
+            var result = manager.GetById(1);
+
+            Assert.IsNotNull(result.Result);
+            Assert.IsTrue(result.Result == testDto);
+            _mockDataContractMapper.Verify(m => m.MapEntityToDto(testEntity), Times.Once);
+        }
     }
 }
