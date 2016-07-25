684a830168cb87e2e0f070a194363da4ec3199dc
FranklyCoded <peter.frank@franklycoded.com>
2016-07-25 01:22:37 +0100

    Changed publish output to release Upped Crud.Net version

--- cicd/kryptonapi.publish.rb
+++ cicd/kryptonapi.publish.rb
@@ -6,11 +6,11 @@ dest = ARGV[0];
 
 puts "Starting publish..."
 Dir.chdir("../src/KryptonAPI") do
-    system "dotnet publish --no-build"
+    system "dotnet publish -c Release --no-build"
 end
 
 puts "Finished publish"
 puts "Copying artifacts to #{dest}"
-FileUtils.cp_r("../src/KryptonAPI/bin/Debug/netcoreapp1.0/publish/.", "#{dest}")
-FileUtils.cp_r("../src/KryptonAPI/bin/Debug/netcoreapp1.0/kryptonapi.db", "#{dest}")
+FileUtils.cp_r("../src/KryptonAPI/bin/Release/netcoreapp1.0/publish/.", "#{dest}")
+FileUtils.cp_r("../src/KryptonAPI/bin/Release/netcoreapp1.0/kryptonapi.db", "#{dest}")
 puts "Finished copying artifacts to #{dest}"
